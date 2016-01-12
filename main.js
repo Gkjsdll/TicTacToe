7d6d620cc12c9de313f40842e32ebe641f5fe7a4
Zack Winchell <zackwinchell@gmail.com>
2016-01-11 19:12:51 -0800

    layed out winCheck skeleton

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
   var $whoseTurn = $('#whoseTurn');
   var $squares = $('.square');
   var spacesRemaining = 9;
-
   newGame();
   $squares.click(squareClickHandler);
 
@@ -19,8 +18,7 @@ $(document).ready(function(){
     var $this = $(this);
     if($this.text() === ""){
       $this.text(whoseTurn);
-      switchTurn();
-      checkWin();
+      checkWin($this);
     }
     else{
       alert("Space already taken");
@@ -45,11 +43,36 @@ $(document).ready(function(){
 
   function checkWin($square){
     spacesRemaining--;
+    var location = $square.index()+1;
+    switch(location){
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+      case 7:
+        break;
+      case 8:
+        break;
+      case 9:
+        break;
+      default:
+        switchTurn();
+        break;
 
+    }
 
     if(!spacesRemaining){
       alert("Game Over!\nIt's a draw!");
     }
+    switchTurn();
   };
 
   function writeTurn(){
